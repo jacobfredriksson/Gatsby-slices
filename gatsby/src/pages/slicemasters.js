7e988676610629dc58948a298d68f3848a0af98b
Jacob Fredriksson <jacobfredriksson@live.se>
2020-11-24 08:27:15 +0100

    🎨 Added pagination

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {graphql, Link} from 'gatsby';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import { Pagination } from '../components/Pagination'
 
 const SlicemasterGrid = styled.div`
   display: grid;
@@ -39,21 +40,28 @@ const SlicemasterStyles = styled.div`
 `
 
 
-export default function SlicemastersPage({data}) {
+export default function SlicemastersPage({data, pageContext}) {
   const slicemasters = data.slicemasters.nodes
-  console.log(slicemasters)
+  console.log({pageContext})
   return (
     <>
+    <Pagination
+      pageSize={parseInt(process.env.GATSBY_PAGE_SIZE)}
+      totalCount={data.slicemasters.totalCount}
+      currentPage={pageContext.currentPage || 1}
+      skip={pageContext.skip}
+      base="/slicemasters"
+    />
      <SlicemasterGrid>
        {slicemasters.map(person => (
-         <SlicemasterStyles>
+         <SlicemasterStyles key={person.id}>
            <Link to={`/slicemaster/${person.slug.current}`}>
             <h2>
               <span className="mark"> {person.name} </span>
             </h2>
            </Link>
            <Img fluid={person.image.asset.fluid} />
-       <p class="description"> {person.description}</p>
+          <p className="description"> {person.description}</p>
          </SlicemasterStyles>
        ))}
      </SlicemasterGrid>
@@ -62,8 +70,8 @@ export default function SlicemastersPage({data}) {
 }
 
 export const query = graphql`
-  query {
-    slicemasters: allSanityPerson {
+  query($skip: Int = 0, $pageSize: Int = 2) {
+    slicemasters: allSanityPerson(limit: $pageSize, skip: $skip) {
       totalCount
       nodes {
         name
